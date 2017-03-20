c6a27473f62ff6142eb3e00ee9e318d15ce909f7
Chonlasith Jucksriporn <chonla@capabilify.com>
2017-03-20 17:52:45 +0700

    introduce primeng and remove unused components/scripts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,30 +2,50 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
+import { CalendarModule, InputTextareaModule, InputTextModule, ChipsModule, FileUploadModule } from 'primeng/primeng';
 
 import { HatchMetaConfig } from './config/hatchmeta.config';
 import { AppComponent } from './components/app/app.component';
 
 import { AutoFormComponent } from './components/autoform/autoform.component';
 import { AutoFieldComponent } from './components/autofield/autofield.component';
-import { DatetimePickerComponent } from './components/datetime-picker/datetime-picker.component';
-import { TagsInputComponent } from './components/tags-input/tags-input.component';
 
 import { FieldToLabelPipe } from './pipes/fieldToLabel/fieldToLabel.pipe';
+
+const appRoutes: Routes = [
+  // { path: 'crisis-center', component: CrisisListComponent },
+  // { path: 'hero/:id',      component: HeroDetailComponent },
+  // {
+  //   path: 'heroes',
+  //   component: HeroListComponent,
+  //   data: { title: 'Heroes List' }
+  // },
+  // { path: '',
+  //   redirectTo: '/heroes',
+  //   pathMatch: 'full'
+  // },
+  { path: 'form/:entity', component: AutoFormComponent },
+  { path: '**', component: AutoFormComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     AutoFormComponent,
     AutoFieldComponent,
-    DatetimePickerComponent,
-    TagsInputComponent,
     FieldToLabelPipe
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes),
+    CalendarModule,
+    InputTextModule,
+    InputTextareaModule,
+    ChipsModule,
+    FileUploadModule
   ],
   providers: [
     HatchMetaConfig
